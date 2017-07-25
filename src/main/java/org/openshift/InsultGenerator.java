7b2a016b372f4d799d8efd43325c8334ac6df937
Kyutae Park <visavis2k@gmail.com>
2017-07-25 18:33:20 +0900

    Added postgres insult

--- src/main/java/org/openshift/InsultGenerator.java
+++ src/main/java/org/openshift/InsultGenerator.java
@@ -16,19 +16,16 @@ public class InsultGenerator {
 			databaseURL += "/" + System.getenv("POSTGRESQL_DATABASE");
 			String username = System.getenv("POSTGRESQL_USER");
 			String password = System.getenv("PGPASSWORD");
-			Connection connection = DriverManager.getConnection(databaseURL, username,
-				password);
+			Connection connection = DriverManager.getConnection(databaseURL, username, password);
 			if (connection != null) {
-				String SQL = "select a.string AS first, b.string AS second, c.string AS noun
-				from short_adjective a, long_adjective b, noun c ORDER BY random() limit 1 ";
+				String SQL = "select a.string AS first, b.string AS second, c.string AS noun from short_adjective a, long_adjective b, noun c ORDER BY random() limit 1 ";
 				Statement stmt = connection.createStatement();
 				ResultSet rs = stmt.executeQuery(SQL);
 				while (rs.next()) {
 					if (vowels.indexOf(rs.getString("first").charAt(0)) == -1) {
 						article = "a";
 					}
-					theInsult = String.format("Thou art %s %s %s %s!", article,
-						rs.getString("first"), rs.getString("second"), rs.getString("noun"));
+					theInsult = String.format("Thou art %s %s %s %s!", article, rs.getString("first"), rs.getString("second"), rs.getString("noun"));
 				}
 				rs.close();
 				connection.close();
